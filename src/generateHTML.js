b3acca8c7c2009f7172031068220fb09724b598e
Yen Nguyen <112992331+yhnguyen720@users.noreply.github.com>
2023-01-31 14:36:06 -0600

    Update generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,81 +1,114 @@
-function manager() {
-    return`
-    <div class="column is-one-third">
-        <div class="card" id="manager">
-            <div class="card-header has-background-info has-text-white p-2">
-                <div class="media">
-                    <div class="media-left">
-                        <i class="material-icons md-48">star</i>
-                    </div>
-                    <div class="media-content">
-                        <p class="title is-4 has-text-white" id="name"> name </p>
-                        <p class="subtitle is-6 has-text-white" id="role"> Manager </p>
+const Employee = require("../lib/Employee");
+
+//create team cards 
+const teamHTML = (team) => {
+
+    //create manager card
+    const managerHTML =  (manager) => 
+        `
+        <div class="column is-one-third">
+            <div class="card employee-card">
+                <div class="card-header has-background-info has-text-white p-2 employee-header">
+                    <div class="media">
+                        <div class="media-left">
+                            <i class="material-icons md-48">star</i>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4 has-text-white">${manager.name}</p>
+                            <p class="subtitle is-6 has-text-white">Manager</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="card-content">
-                <p id="id"> ID </p>
-                <p id="email"> email </p>
-                <p id="office-number"> office number </p> 
-            </div>
-        </div>
-    </div>
-    `
-}
-
-function engineer() {
-    return `
-    <div class="column is-one-third">
-        <div class="card" id="engineer">
-            <div class="card-header has-background-primary has-text-white p-2">
-                <div class="media">
-                    <div class="media-left">
-                        <i class="material-icons md-48">rocket</i>
-                    </div>
-                    <div class="media-content">
-                        <p class="title is-4 has-text-white" id="name"> name </p>
-                        <p class="subtitle is-6 has-text-white" id="role"> Engineer </p>
-                    </div>
+                <div class="card-content">
+                    <p>ID Number: ${manager.id}</p>
+                    <p>Email Address: <a href="mailto:${manager.email}">${manager.email}</p>
+                    <p>Office Number: ${manager.office}</p> 
                 </div>
             </div>
-            <div class="card-content">
-                <p id="id"> ID </p>
-                <p id="email"> email </p>
-                <p id="github"> github </p> 
-            </div>
         </div>
-    </div>
-    `
-}
+        `
+    ;
 
-function intern() {
-    return `
-    <div class="column is-one-third">
-        <div class="card" id="intern">
-            <div class="card-header has-background-success has-text-white p-2">
-                <div class="media">
-                    <div class="media-left">
-                        <i class="material-icons md-48">school</i>
-                    </div>
-                    <div class="media-content">
-                    <p class="title is-4 has-text-white" id="name"> name </p>
-                    <p class="subtitle is-6 has-text-white" id="role"> Intern </p>
+    //create engineer card
+    const engineerHTML =  (engineer) => 
+        `
+        <div class="column is-one-third">
+            <div class="card employee-card">
+                <div class="card-header has-background-info has-text-white p-2 employee-header">
+                    <div class="media">
+                        <div class="media-left">
+                            <i class="material-icons md-48">rocket</i>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4 has-text-white">${engineer.name}</p>
+                            <p class="subtitle is-6 has-text-white">Engineer</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="card-content">
-                <p id="id"> ID </p>
-                <p id="email"> email </p>
-                <p id="school"> school </p> 
+                <div class="card-content">
+                    <p>ID Number: ${engineer.id}</p>
+                    <p>Email Address: <a href="mailto:${engineer.email}">${engineer.email}</p>
+                    <p>GitHub: <a href="https://github.com/${engineer.gitHub}" target="_blank"
+                    rel="noopener noreferrer">${engineer.github}</p> 
+                </div>
             </div>
         </div>
-    </div>
-    `
+        `
+        ;
+
+        //create intern card
+        const internHTML =  (intern) => 
+        `
+        <div class="column is-one-third">
+            <div class="card employee-card">
+                <div class="card-header has-background-info has-text-white p-2 employee-header">
+                    <div class="media">
+                        <div class="media-left">
+                            <i class="material-icons md-48">school</i>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4 has-text-white">${intern.name}</p>
+                            <p class="subtitle is-6 has-text-white">Intern</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-content">
+                    <p>Id Number: ${intern.id}</p>
+                    <p>Email Address: <a href="mailto:${intern.email}">${intern.email}</p>
+                    <p>School: ${intern.school}</p> 
+                </div>
+            </div>
+        </div>
+        `
+        ;
+
+        const html = [];
+
+        html.push(
+            team
+            .filter((employee) => employee.getRole() === "Manager")
+            .map((manager) => managerHTML(manager))
+            .join("")
+        );
+
+        html.push(
+            team
+            .filter((employee) => employee.getRole() === "Engineer")
+            .map((engineer) => engineerHTML(engineer))
+            .join("")
+        );
+
+        html.push(
+            team.filter((employee) => employee.getRole() === "Intern")
+            .map((intern) => internHTML(intern))
+            .join("")
+        );
 }
 
 
-function generateHTML() {
-    return`
+//create main body html
+const mainHTML = (team) => 
+    `
     <!DOCTYPE html>
     <html lang="en">
 
@@ -102,11 +135,12 @@ function generateHTML() {
         <section id="team-container">
             <div class="container is-fluid">
                 <div class="columns is-multiline">
-
-
+                ${teamHTML(team)}
                 </div>
             </div>           
         </section>
     </body>
     `
-}
+    ;
+
+module.exports = mainHTML;
